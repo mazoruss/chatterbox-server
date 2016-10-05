7c849c2d0d3d0cdf8f569d3ac94d414b11299a0c
mazoruss <chesszhu@gmail.com>
2016-10-04 17:19:18 -0700

    Refactored to parse POST request

--- server/expressive.js
+++ server/expressive.js
@@ -1,15 +1,40 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 var handler = require('./request-handler2');
 
-app.options('/classes/messages', handler.requestHandler);
-app.get('/classes/messages', handler.requestHandler);
-app.post('/classes/messages', handler.requestHandler);
+//app.use(function1)
+var addHeader = function(req, res, next) {
+
+  res.set('access-control-allow-origin', '*');
+  res.set('access-control-allow-methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.set('access-control-allow-headers', 'content-type, accept');
+  res.set('access-control-max-age', 10 );
+  res.set('Content-Type', 'application/json');
+
+  next();
+};
 
 app.use(express.static('/Users/student/Desktop/2016-09-chatterbox-server/client/client/'));
 
+app.use(addHeader);
+app.use(bodyParser());
+app.get('/classes/messages', handler.requestHandler);
+app.post('/classes/messages', handler.requestHandler);
+// app.options('/classes/messages', handler.requestHandler);
+
+
 app.listen(3005, function() {
-  console.log('hi i am listening');
+  console.log('You\'ve been hacked!');
 });
+
+
+
+  // var responseBody = {
+  //   headers: headers,
+  //   method: request.method,
+  //   url: request.url,
+  //   results: []
+  // };
--- server/request-handler2.js
+++ server/request-handler2.js
@@ -1,18 +1,7 @@
 var fs = require('fs');
 var path = require('path');
-/*************************************************************
+// var bodyParser = require('bodyParser');
 
-You should implement your request handler function in this file.
-
-requestHandler is already getting passed to http.createServer()
-in basic-server.js, but it won't work as is.
-
-You'll have to figure out a way to export this function from
-this file and include it in basic-server.js so that it actually works.
-
-*Hint* Check out the node module documentation at http://nodejs.org/api/modules.html.
-
-**************************************************************/
 var defaultCorsHeaders = {
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
@@ -22,80 +11,55 @@ var defaultCorsHeaders = {
 
 // Set up the headers and body
 
-var requestHandler = function(request, response) {
+var actions = {
+  GET: function(request, response, responseBody ) {
+    response.status(200);
+    fs.readFile('messages.txt', (err, data) => {
+      responseBody.results = JSON.parse(data);
+      response.end(JSON.stringify(responseBody));
+    });
+  }, 
+  POST: function(request, response, responseBody) {
+    statusCode = 201;
+    
+    fs.readFile('messages.txt', (err, data) => {
+      //Get the currently stored messages
+      // console.log('req body', request.body);
+      var messageData = JSON.parse(data.toString());
+      var newMessage = JSON.parse(Object.keys(request.body)[0]);
 
-  console.log(request.url);
+      newMessage.createdAt = new Date();
+      
+      //Add the new message to the messages array and write it to the file
+      messageData.unshift(newMessage);
+      fs.writeFile('messages.txt', JSON.stringify(messageData), (err) => {
+      });
+      response.end();
+    });
+  },
+  OPTIONS: function(request, response, responseBody) {
+    response.status(200);
+
+    fs.readFile('messages.txt', (err, data) => {
+      responseBody.results = JSON.parse(data);
+      response.end(JSON.stringify(responseBody));
+    });
+  }
+};
+
+var requestHandler = function(request, response) {
   
-  var headers = defaultCorsHeaders;
-  headers['Content-Type'] = 'application/json';
 
   var responseBody = {
-    headers: headers,
     method: request.method,
     url: request.url,
     results: []
   };
 
   console.log('Serving request type ' + request.method + ' for url ' + request.url);
-  var str = '';
-  //Create the status codes
-  var statusCode = 404;
 
-  
-  if (request.url === '/classes/messages' && (request.method === 'GET' || request.method === 'OPTIONS')) {
-    //===================================
-    //============Get request============
-    //===================================
-    statusCode = 200;
-    response.writeHead(statusCode, headers);
-
-    fs.readFile('messages.txt', (err, data) => {
-      //Get the currently stored messages
-      var messageData = JSON.parse(data);
-
-      responseBody.results = messageData;
-      response.end(JSON.stringify(responseBody));
-    });
-
-
-  } else if (request.url === '/classes/messages' && request.method === 'POST') {
-    //===================================
-    //===========Post request============
-    //===================================
-    statusCode = 201;
-    //Get request and turn it into a string
-    request.on('data', function(data) {
-      str += data;
-    });
-    
-    request.on('error', function(err) {
-      console.log(error);
-    });
-
-    request.on('end', function() {
-      fs.readFile('messages.txt', (err, data) => {
-        
-        //Get the currently stored messages
-        var messageData;
-        messageData = JSON.parse(data.toString());
-        var newMessage = JSON.parse(str);
-        newMessage.createdAt = new Date();
-        
-        //Add the new message to the messages array and write it to the file
-        messageData.unshift(newMessage);
-        fs.writeFile('messages.txt', JSON.stringify(messageData), (err) => {
-        });
-
-        response.writeHead(statusCode, headers);
-        response.end();
-      });
-
-    });
-
-  } else {
-    response.writeHead(statusCode, headers);
-    response.end();
-  }
+  var action = actions[request.method];
+  action(request, response, responseBody);
 
 };
 
